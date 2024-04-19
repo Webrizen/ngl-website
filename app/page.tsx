5142d129d0d6faf826f94f58a842fee2f11f3109
WebRizen <webrizen@gmail.com>
2024-04-19 17:21:41 +0530

    Comment out `<Projects />` component in Home page
    This commit comments out the `<Projects />` component in the Home page of the application. The component is wrapped in a conditional comment, `{/* <Projects

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Encryption />
         <Services />
         <Features />
-        <Projects />
+        {/* <Projects /> */}
         <Testimonial />
       </div>
     </main>
